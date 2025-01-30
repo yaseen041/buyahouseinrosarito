357142110ade4992a1790f08d8724ed74b391e79
amirch357 <yaseen3327095758@gmail.com>
2025-01-30 17:40:36 +0500

    working on tables

--- app/components/SizeCalculeter/index.jsx
+++ app/components/SizeCalculeter/index.jsx
@@ -9,21 +9,21 @@ const HomeSizeCalculator = () => {
     return (
         <div className=" table-container mt-4 h-100">
             {/* Section 1: Conversion from M² to Ft² */}
-            <table className="table m-0" style={{ height: "23.5%" }} >
+            <table className="table m-0" style={{ height: "26.6%" }} >
                 <thead className="" style={{ backgroundColor: "#699A5B" }} >
                     <tr>
-                        <th colSpan="2" className="text-center align-middle">
+                        <th colSpan="2" className="text-center align-middle ">
                             <p style={{ color: "#FFF" }}  > Estimate Your New Home Size in Square Feet (Using Mexico's Meters Squared)</p>
                         </th>
                     </tr>
                     <tr>
-                        <th className="text-center align-middle"><p style={{ color: "#FFF" }}  >Input (M²)</p></th>
-                        <th className="text-center align-middle"><p style={{ color: "#FFF" }}  >Square Feet (Ft²)</p></th>
+                        <th className="text-center align-middle "><p style={{ color: "#FFF" }}  >Input (M²)</p></th>
+                        <th className="text-center align-middle "><p style={{ color: "#FFF" }}  >Square Feet (Ft²)</p></th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td className="text-center align-middle" style={{borderBottom:0,backgroundColor:"#f7f7f7"}} >
+                        <td className="text-center align-middle " style={{borderBottom:0,backgroundColor:"#f7f7f7"}} >
 
                             <input
                                 style={{
@@ -39,7 +39,7 @@ const HomeSizeCalculator = () => {
                             />
 
                         </td>
-                        <td className="text-center align-middle" style={{borderBottom:0,backgroundColor:"#f7f7f7"}} > <p> {sqm ? (sqm * 10.764).toFixed(2) : "—"}</p></td>
+                        <td className="text-center align-middle " style={{borderBottom:0,backgroundColor:"#f7f7f7"}} > <p> {sqm ? (sqm * 10.764).toFixed(2) : "—"}</p></td>
                     </tr>
                 </tbody>
             </table>
@@ -48,19 +48,19 @@ const HomeSizeCalculator = () => {
             <table className="table m-0" style={{ height: "23.5%" }} >
                 <thead className="" style={{ backgroundColor: "#124773" }} >
                     <tr>
-                        <th colSpan="3" className="text-center align-middle">
+                        <th colSpan="3" className="text-center align-middle ">
                             <p style={{ color: "#FFF" }} > Find Your Ideal Home Size for Just $110 Per Square Foot in Mexico (No Ocean View)</p>
                         </th>
                     </tr>
                     <tr>
-                        <th className="text-center align-middle"> <p style={{ color: "#FFF" }} >Input (Budget $)</p></th>
-                        <th className="text-center align-middle"><p style={{ color: "#FFF" }} >Output (Size in Ft²)</p></th>
-                        <th className="text-center align-middle"><p style={{ color: "#FFF" }} >Output (Size in M²)</p></th>
+                        <th className="text-center align-middle "> <p style={{ color: "#FFF" }} >Input (Budget $)</p></th>
+                        <th className="text-center align-middle "><p style={{ color: "#FFF" }} >Output (Size in Ft²)</p></th>
+                        <th className="text-center align-middle "><p style={{ color: "#FFF" }} >Output (Size in M²)</p></th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td className="text-center align-middle" style={{borderBottom:0,backgroundColor:"#f7f7f7"}}  >
+                        <td className="text-center align-middle " style={{borderBottom:0,backgroundColor:"#f7f7f7"}}  >
                             <input
                                 style={{
                                     fontSize: "1.5rem", appearance: "textfield",
@@ -74,8 +74,8 @@ const HomeSizeCalculator = () => {
                                 placeholder="Enter Budget ($)"
                             />
                         </td>
-                        <td className="text-center align-middle" style={{borderBottom:0,backgroundColor:"#f7f7f7"}} > <p> {budget110 ? (budget110 / 110).toFixed(2) : "—"}</p></td>
-                        <td className="text-center align-middle" style={{borderBottom:0,backgroundColor:"#f7f7f7"}} > <p> {budget110 ? ((budget110 / 110) / 10.764).toFixed(2) : "—"}</p></td>
+                        <td className="text-center align-middle " style={{borderBottom:0,backgroundColor:"#f7f7f7"}} > <p> {budget110 ? (budget110 / 110).toFixed(2) : "—"}</p></td>
+                        <td className="text-center align-middle " style={{borderBottom:0,backgroundColor:"#f7f7f7"}} > <p> {budget110 ? ((budget110 / 110) / 10.764).toFixed(2) : "—"}</p></td>
                     </tr>
                 </tbody>
             </table>
@@ -84,19 +84,19 @@ const HomeSizeCalculator = () => {
             <table className="table m-0" style={{ height: "23.5%" }} >
                 <thead className="" style={{ backgroundColor: "#2EC6F3" }} >
                     <tr>
-                        <th colSpan="3" className="text-center align-middle">
+                        <th colSpan="3" className="text-center align-middle ">
                             <p style={{ color: "#FFF" }} >Affordable Elegance: Calculate Your Ocean View Home Size at $300 Per Square Foot</p>
                         </th>
                     </tr>
                     <tr>
-                        <th className="text-center align-middle"><p style={{ color: "#FFF" }} >Input (Budget $)</p></th>
-                        <th className="text-center align-middle"><p style={{ color: "#FFF" }} >Output (Size in Ft²)</p></th>
-                        <th className="text-center align-middle"><p style={{ color: "#FFF" }} >Output (Size in M²)</p></th>
+                        <th className="text-center align-middle "><p style={{ color: "#FFF" }} >Input (Budget $)</p></th>
+                        <th className="text-center align-middle "><p style={{ color: "#FFF" }} >Output (Size in Ft²)</p></th>
+                        <th className="text-center align-middle "><p style={{ color: "#FFF" }} >Output (Size in M²)</p></th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td className="text-center align-middle"  style={{borderBottom:0,backgroundColor:"#f7f7f7"}}>
+                        <td className="text-center align-middle "  style={{borderBottom:0,backgroundColor:"#f7f7f7"}}>
                             <input
                                 style={{
                                     fontSize: "1.5rem", appearance: "textfield",
@@ -110,8 +110,8 @@ const HomeSizeCalculator = () => {
                                 placeholder="Enter Budget ($)"
                             />
                         </td>
-                        <td className="text-center align-middle"  style={{borderBottom:0,backgroundColor:"#f7f7f7"}}><p>{budget300 ? (budget300 / 300).toFixed(2) : "—"}</p></td>
-                        <td className="text-center align-middle" style={{borderBottom:0,backgroundColor:"#f7f7f7"}} ><p>{budget300 ? ((budget300 / 300) / 10.764).toFixed(2) : "—"}</p></td>
+                        <td className="text-center align-middle "  style={{borderBottom:0,backgroundColor:"#f7f7f7"}}><p>{budget300 ? (budget300 / 300).toFixed(2) : "—"}</p></td>
+                        <td className="text-center align-middle " style={{borderBottom:0,backgroundColor:"#f7f7f7"}} ><p>{budget300 ? ((budget300 / 300) / 10.764).toFixed(2) : "—"}</p></td>
                     </tr>
                 </tbody>
             </table>
@@ -120,19 +120,19 @@ const HomeSizeCalculator = () => {
             <table className="table m-0" style={{ height: "23.5%" }} >
                 <thead className="" style={{ backgroundColor: "#E8233F" }} >
                     <tr>
-                        <th colSpan="3" className="text-center align-middle">
+                        <th colSpan="3" className="text-center align-middle ">
                             <p style={{ color: "#FFF" }} > Luxury Living: Discover Your Beach Front Home Size at $500 Per Square Foot</p>
                         </th>
                     </tr>
                     <tr>
-                        <th className="text-center align-middle"><p style={{ color: "#FFF" }} >Input (Budget $)</p></th>
-                        <th className="text-center align-middle"><p style={{ color: "#FFF" }} >Output (Size in Ft²)</p></th>
-                        <th className="text-center align-middle"><p style={{ color: "#FFF" }} >Output (Size in M²)</p></th>
+                        <th className="text-center align-middle "><p style={{ color: "#FFF" }} >Input (Budget $)</p></th>
+                        <th className="text-center align-middle "><p style={{ color: "#FFF" }} >Output (Size in Ft²)</p></th>
+                        <th className="text-center align-middle "><p style={{ color: "#FFF" }} >Output (Size in M²)</p></th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td className="text-center align-middle" style={{backgroundColor:"#f7f7f7"}} >
+                        <td className="text-center align-middle " style={{backgroundColor:"#f7f7f7"}} >
                             <input
                                 style={{
                                     fontSize: "1.5rem", appearance: "textfield",
@@ -146,8 +146,8 @@ const HomeSizeCalculator = () => {
                                 placeholder="Enter Budget ($)"
                             />
                         </td>
-                        <td className="text-center align-middle" style={{backgroundColor:"#f7f7f7"}}  ><p>{budget500 ? (budget500 / 500).toFixed(2) : "—"}</p></td>
-                        <td className="text-center align-middle" style={{backgroundColor:"#f7f7f7"}}  ><p>{budget500 ? ((budget500 / 500) / 10.764).toFixed(2) : "—"}</p></td>
+                        <td className="text-center align-middle " style={{backgroundColor:"#f7f7f7"}}  ><p>{budget500 ? (budget500 / 500).toFixed(2) : "—"}</p></td>
+                        <td className="text-center align-middle " style={{backgroundColor:"#f7f7f7"}}  ><p>{budget500 ? ((budget500 / 500) / 10.764).toFixed(2) : "—"}</p></td>
                     </tr>
                 </tbody>
             </table>
